e0077be4c191b214818b9ca8b87c69ddd6152218
Matias Zahn <zahnmatias14@gmail.com>
2023-12-03 03:07:03 +0000

    Seguir con los Modules

---
+++ src/config/database/database.js
@@ -0,0 +1,24 @@
+import { Sequelize } from 'sequelize';
+import { envs } from '../enviroments/enviroments.js';
+
+export const sequelize = new Sequelize(envs.DB_URI, {
+  logging: false,
+});
+
+export async function authenticate() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established succesfully. 📱');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function syncUp() {
+  try {
+    await sequelize.sync();
+    console.log('Connection has been synced succesfully. 🐲');
+  } catch (error) {
+    console.error(error);
+  }
+}
